148838dbcc53bc4041315f804a52d5b319dcec2d
fc-webide <fc-webide@ali.com>
2023-02-20 15:45:16 +0000

    "save function with codechecksum 17067371809680201890"

--- index.py
+++ index.py
@@ -35,7 +35,7 @@ def generate_text():
         # print("\n")
         
         
-@app.route('/',methods=['POST'])
+@app.route('/',methods=['POST','GET'])
 def stream_text():
     """流式传输文本的视图函数"""
     return Response(generate_text(), mimetype='text/plain', content_type='text/event-stream')
