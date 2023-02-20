44c66141c8672d9328724091698ad21443f0d990
fc-webide <fc-webide@ali.com>
2023-02-20 14:35:04 +0000

    "save function with codechecksum 12635901948941183447"

--- index.py
+++ index.py
@@ -1,11 +1,3 @@
-'''
-Author: xiaoyichao xiao_yi_chao@163.com
-Date: 2023-02-20 20:59:52
-LastEditors: xiaoyichao xiao_yi_chao@163.com
-LastEditTime: 2023-02-20 21:06:11
-FilePath: /flask-code/http_test.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
 import os 
 import sys
 import time
@@ -24,32 +16,15 @@ chatbot = Chatbot(config={
     })
 print("创建bot成功")
 end_time = time.time()
-print("111",end_time-begin_time)
 
-print("开始请求bot")
-begin_time = time.time()
-# prompt = "你是谁"
 
-# prev_text = ""
-# for data in chatbot.ask(
-#     prompt,
-# ):
-#     # response = data["message"]
-#     message = data["message"][len(prev_text) :]
-#     # print(message, end="", flush=True)
-#     prev_text = data["message"]
-#     # print(prev_text)
-# # print()
-# end_time = time.time()
-# print("222", end_time-begin_time)
+app = Flask(__name__)
 
-@app.route("/message", methods=["POST"])
-def streaming_file():
-    """
-    流式发送文件
-    @return:
-    """
-    msg = request.json.get('msg')
+def generate_text():
+    """生成文本的生成器函数"""
+    # for i in range(10):
+    #     yield f"这是第{i}行文本\n"
+    msg = "你是谁"
     prev_text = ""
     for data in chatbot.ask(
         msg,
@@ -57,18 +32,13 @@ def streaming_file():
         # response = data["message"]
         message = data["message"][len(prev_text) :]
         # print(message, end="", flush=True)
-        prev_text = data["message"]
-        # print(prev_text)
+        yield message
+        
 
-        data = message
-        if not data:
-            break
-        yield data
+@app.route('/')
+def stream_text():
+    """流式传输文本的视图函数"""
+    return Response(generate_text(), mimetype='text/plain', content_type='text/event-stream')
 
-    response = Response(send_file_fp(), content_type='multipart/form-data; boundary=something')
-    # response.headers["Content-disposition"] = 'attachment; filename={}'.format(params.get("filename"))
-    # response.headers["Content-length"] = fsize
-    return response
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+# 在这个例子中，`generate_text` 函数是一个生成器函数，它会生成一些文本行。`stream_text` 函数是一个 Flask 视图函数，它返回一个流式响应，该响应将调用 `generate_text` 生成的文本行逐行发送给客户端。
