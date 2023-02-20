c33d26a6566fbcd41d37bf964fc4c1d887448153
fc-webide <fc-webide@ali.com>
2023-02-20 15:26:49 +0000

    "save function with codechecksum 10887490587558290583"

--- index.py
+++ index.py
@@ -20,43 +20,43 @@ end_time = time.time()
 
 app = Flask(__name__)
 
-# def generate_text():
-#     """生成文本的生成器函数"""
-#     # for i in range(10):
-#     #     yield f"这是第{i}行文本\n"
-#     # msg = request.form.get('msg')
-#     # msg = "你是谁"
-#     prev_text = ""
-#     for data in chatbot.ask(
-#         msg,
-#     ):
-#         # response = data["message"]
-#         message = data["message"][len(prev_text) :]
-#         # print(message, end="", flush=True)
-#         yield message
-#         prev_text = data["message"]
-#         # print("\n")
-        
-        
-# @app.route('/',methods=['POST'])
-# def stream_text():
-#     """流式传输文本的视图函数"""
-#     return Response(generate_text(), mimetype='text/plain', content_type='text/event-stream')
-
-
-
-from flask import Flask, Response, request
-app = Flask(__name__)
-
 def generate_text(request):
-    msg = request.form.get('msg')
-    prev_text = ""
-    with app.app_context():
-        for data in chatbot.ask(msg):
-            message = data["message"][len(prev_text):]
-            yield message
-            prev_text = data["message"]
+    """生成文本的生成器函数"""
+    # msg = request.form.get('msg')
+    msg = request.args.get("msg")
 
-@app.route('/', methods=['POST'])
+    # msg = "你是谁"
+    prev_text = ""
+    for data in chatbot.ask(
+        msg,
+    ):
+        # response = data["message"]
+        message = data["message"][len(prev_text) :]
+        # print(message, end="", flush=True)
+        yield message
+        prev_text = data["message"]
+        # print("\n")
+        
+        
+@app.route('/',methods=['POST'])
 def stream_text():
+    """流式传输文本的视图函数"""
     return Response(generate_text(request), mimetype='text/plain', content_type='text/event-stream')
+
+
+
+# from flask import Flask, Response, request
+# app = Flask(__name__)
+
+# def generate_text(request):
+#     msg = request.form.get('msg')
+#     prev_text = ""
+#     with app.app_context():
+#         for data in chatbot.ask(msg):
+#             message = data["message"][len(prev_text):]
+#             yield message
+#             prev_text = data["message"]
+
+# @app.route('/', methods=['POST'])
+# def stream_text():
+#     return Response(generate_text(request), mimetype='text/plain', content_type='text/event-stream')
