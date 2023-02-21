15c7f83d3d4d709776d18ec2d2cc274013c7b7c7
fc-webide <fc-webide@ali.com>
2023-02-21 05:37:06 +0000

    "save function with codechecksum 9798200999274578252"

--- index.py
+++ index.py
@@ -218,6 +218,7 @@ def infocheck(text,openid):
 
         text = text.replace("\n", "")
         text = text.replace("\"", "")
+        text = text.replace("\'", "")
 
         data = '{"content": "' + text + '","openid": "' + openid + '","scene":  2 ,"version":  2 }'
         headers = {'Content-Type': 'application/json'}
