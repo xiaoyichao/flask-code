4e54cadfdbf9531f7eab1756088d08134ba5f3e0
fc-webide <fc-webide@ali.com>
2023-02-24 11:50:20 +0000

    "save function with codechecksum 3309933673472127450"

--- index.py
+++ index.py
@@ -9,7 +9,7 @@ import pymysql
 import random
 from revChatGPT.V1 import Chatbot
 import time
-from creat_bot import creat_new_bot
+# from creat_bot import creat_new_bot
 
 account_dict = {'wolhtetjuywvb@eurokool.com': 'abc123456789', 'uvffqudbqob@eurokool.com': 'abc123456789', 'svakzkwcegq@eurokool.com': 'abc123456789', 'wyabuwztdcct@eurokool.com': 'abc123456789', 'qmqows@eurokool.com': 'abc123456789', 'lahuseman88@outlook.com': 'of75stFg8j', 'hp52477bgmw7027@163.com': 'XnwlHh5VwD', 'px303xgza2591@163.com': 'GMV1x4wtQC', 'oxqvp331ztx1@163.com': '2RUt5HOjbh', 'at10574llm37@163.com': 'LE4cy7hHQt', 'ijyvnc31uj72@163.com': '68s00wDGmV', 'sf7549vvvd9429@163.com': '3PXQrl4eEa', 'qdmcca89jt466@163.com': 'c618XMONr7', 'uyprjj68cb372@163.com': 'B3P21G878j', 'vi8715ewcu3894@163.com': 'N4v99fzTE4', 'gwclko009sa211@163.com': '29uQ60aFUi', 'ppsor13xvq33@163.com': 'z4yIqD2wQX', 'zy673rowg952@163.com': 'ZX2P8w9A2L', 'ol5092rxmn1381@163.com': 'T7SU1VVNoX', 'aapqp508udid12@163.com': '48aSapa33T', 'kwhaf497rru7@163.com': 'reG0TRUM38', 'yejiw31tnl3@163.com': 'rLKCWrLdGh', 'shrfm51brh36@163.com': 'jPx6PQD1s9', 'heail17ewu25@163.com': 'ho4l2A18MU', 'jb6963fdhv062@163.com': 'bQA2s51d9d', 'snjbc8672ytvs37@163.com': 'T10rLTGLC2', 'uozdb01bjl31@163.com': '6zB4lhgg8A', 'iy2373patc1753@163.com': '40g52K2rPf', 'bvpui74kyox04@163.com': 'hEHpWWCWwG', 'slqdi3170ncrg74@163.com': '3PrKE16NpZ', 'dsyqy18esd2@163.com': '4N1ZgWw5Wc', 'injimr95wy023@163.com': 'y6NlU7Ab10', 'jh8994hoyw8778@163.com': 'TPx6S0niJh', 'hllri02sape92@163.com': '10z1o1970a', 'oeixcv048nw06@163.com': 'doL0H0s6pa', 'hb93594chrn527@163.com': 'k4kyKbQ67n', 'ggzlm907nizp30@163.com': 'qx9XYs194p', 'kl19967dgak179@163.com': 's29s2ubPQz', 'zbmhv435rcky46@163.com': 'gp2bb5wWEc', 'ne87280hkrc3035@163.com': '3J72720xz2', 'lm845oyoa384@163.com': 'm55Vh3xXNb', 'shndcl35xb75@163.com': 'YEggHvB8KS', 'ug338xsxs865@163.com': '7E99b553PD', 'hi45728ahlm1616@163.com': '5gRLz2ckZ6', 'kaiucb434fc507@163.com': 'FqzVglS26E', 'rfcxn305gqkw6@163.com': 'N9xhzlq2xM', 'owcmn6032wjlg0@163.com': '0q7OEK5ebd', 'bedupj72vv90@163.com': 'i4nvjQ8Lh8', 'zt0834cbde585@163.com': 'r05a2vb3JX', 'ih7850ejso311@163.com': 'su057OmnIB', 'rhxtyh320kq29@163.com': 'g96YMnuuoa', 'ltbbby14sc643@163.com': 'plDg0kf3vU', 'sinttw48gq102@163.com': '3c1UV449u9', 'rz2532xmlh691@163.com': '29wrkq2XZh', 'nsbzg881ujv0@163.com': 'u50ynaVk4y', 'yi6899kipr246@163.com': 'q1t3uPDJpA'}
 
@@ -59,6 +59,80 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
 
 
+def get_bot(account, password):
+    try_num = 0
+    while try_num<3:
+        try:
+            chatbot = Chatbot(config={
+                "email": account,
+                "password": password,
+                })
+            try_num +=1
+            return chatbot
+        except:
+            try_num +=1
+            print("第%s次尝试创建chatbot"%try_num)
+            print(account,password)
+    return None
+
+def creat_new_bot(account_dict,all_bots,used_bot,usernum):
+    # 随机选择一个没有使用的bot,最多等待5次
+    print("计算没有使用的bots")
+    tmp_bots = list(all_bots - used_bot)
+    if len(tmp_bots)>0:
+        account = random.choice(tmp_bots)
+        password = account_dict[account]
+        
+        chatbot = get_bot(account, password)
+        
+        if chatbot is None:
+            print("创建bot失败，尝试其他账户")
+            for i in range(3):
+                account = random.choice(tmp_bots)
+                password = account_dict[account]
+                chatbot = get_bot(account, password)  
+                used_bot.add(chatbot)
+                if chatbot is not None:
+                    # break
+                    print("随机选择chatbot, done")
+                    return {"chatbot":chatbot,"account_dict":account_dict,"all_bots":all_bots,"used_bot":used_bot,"usernum":usernum}
+            errmsg = "chatgpt的官网登录不上了，请稍后重试"
+            print(errmsg)
+            res = {
+                "resmsg": errmsg,
+                "num": usernum+1,
+                "code": 200
+            }
+            return res
+              
+    else:
+        i = 0
+        while i < 5:
+            time.sleep(0.5)
+            print("开始重试")
+            tmp_bots = list(all_bots - used_bot)
+            i+=1
+            if len(tmp_bots)>0:
+                account = random.choice(tmp_bots)
+                password = account_dict[account]
+                chatbot = get_bot(account, password)
+                used_bot.add(chatbot)
+                print("重试后，选择了 bot")
+                print("随机选择chatbot, done")
+                return {"chatbot":chatbot,"account_dict":account_dict,"all_bots":all_bots,"used_bot":used_bot,"usernum":usernum}
+
+        errmsg = "太多用户使用，导致账号不足"
+        print(errmsg)
+        res = {
+            "resmsg": errmsg,
+            "num": usernum+1,
+            "code": 200
+        }
+        return res
+        
+    
+       
+    
 
 # 数据表
 class BaseConfig(db.Model):
